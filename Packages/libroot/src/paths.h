54a4f742a719f17dfead6d89c45eb2e3d1972297
roothider <158083651+roothider@users.noreply.github.com>
2024-05-08 01:13:22 +0800

    update libroot using libroothide

--- Packages/libroot/src/paths.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBROOT_PATHS_H
-#define LIBROOT_PATHS_H
-
-const char *libroot_get_root_prefix(void);
-const char *libroot_get_jbroot_prefix(void);
-const char *libroot_get_boot_uuid(void);
-
-#endif
